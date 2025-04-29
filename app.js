ce54e73b370736a17dcab545f74db69ebebfa344
MarcosVinicius1051 <marcosluxvinicius@gmail.com>
2025-04-29 10:33:55 -0300

    mongodb connect

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
+import e from "express";
 import express from "express"
+
+
 const app = express(); 
 const PORT= 3031
 
+
+//config
+    //EJS 
+        app.use("view engine", "ejs");
+    //Body-parser
+        app.use(express.urlencoded({extended:true}));
+        app.use(express.json());
+    //Public
+        app.use(express.static("./src/public"));
+    //Rotas
+
+
+//rotas
 app.get("/",(req,res)=>{
 res.send("bem vindo ao servidor")
 });
+app.get("/books",(req,res)=>{
+res.render("books")
+});
+
 
 app.listen(PORT,()=>{
     console.log("servidor ligado")
 });
+
---
+++ src/modules/db.js
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://localhost/bancoLivros").then(()=>{
+    console.log("db mongodb funcionando")
+}).catch((err)=>{
+    console.log("db mongodb error: "+err)
+})
